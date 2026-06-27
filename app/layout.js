73ea788bd680e40e53d43e9db66c7bb630efac2f
Fathismael93 <benew-tech@benew-dj.com>
2026-06-27 21:46:32 +0300

    Implemented icons

--- app/layout.js
+++ app/layout.js
@@ -29,17 +29,13 @@ export const metadata = {
   // AJOUT DE LA CONFIGURATION DES ICÔNES
   icons: {
     icon: [
-      { url: "/icon.ico" },
+      { url: "/favicon.ico" },
       { url: "/icon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/icon-32x32.png", sizes: "32x32", type: "image/png" },
       { url: "/icon-192x192.png", sizes: "192x192", type: "image/png" },
       { url: "/icon-512x512.png", sizes: "512x512", type: "image/png" },
     ],
-    shortcut: ["/icon.ico"],
-    apple: [
-      { url: "/apple-icon.png" },
-      { url: "/apple-icon-180x180.png", sizes: "180x180", type: "image/png" },
-    ],
+    shortcut: ["/favicon.ico"],
     other: [
       {
         rel: "mask-icon",
@@ -55,47 +51,6 @@ export const metadata = {
     email: false,
     address: false,
     telephone: false,
-  },
-  robots: {
-    index: true,
-    follow: true,
-    nocache: true,
-    noimageindex: false,
-    googleBot: {
-      index: true,
-      follow: true,
-      noimageindex: false,
-      "max-image-preview": "large",
-      "max-video-preview": -1,
-      "max-snippet": -1,
-    },
-  },
-  openGraph: {
-    type: "website",
-    locale: "fr_FR",
-    url: SITE_URL,
-    title: "Buy It Now",
-    description:
-      "Boutique en ligne simplifiée (BS), Buy It Now est la solution pour acheter et vendre facilement sur Internet.",
-    siteName: "BS - Buy It Now",
-    // AJOUT D'IMAGES POUR OPEN GRAPH
-    images: [
-      {
-        url: `${SITE_URL}/og-image.jpg`,
-        width: 1200,
-        height: 630,
-        alt: "Buy It Now",
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Buy It Now",
-    description: "Boutique en ligne simplifiée (BS), Buy It Now",
-    creator: "@benew",
-    site: "@benew",
-    // AJOUT D'IMAGES POUR TWITTER
-    images: [`${SITE_URL}/og-image.jpg`],
   },
 };
 
